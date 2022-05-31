9c01c5e0dc55281349eb33f93596dfc775472f9b
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-31 14:15:21 +0300

    [ADD] wallet/view

--- wallet/views/auth.py
+++ wallet/views/auth.py
@@ -1,6 +1,13 @@
+from django.shortcuts import render
 from django.views.generic import DetailView, View
+
+from wallet.forms.auth import LoginForm
 
 class LoginView(View):
     """Login"""
     def get(self, request, *args, **kwargs):
         form = LoginForm(request.POST or None)
+        context = {
+            "form": form
+        }
+        return render(request, 'login.html', context)
