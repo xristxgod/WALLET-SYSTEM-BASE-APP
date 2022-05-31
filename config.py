e977883937e646ac2bc40c5cb4ed8c22524fccde
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-31 11:48:43 +0300

    [ADD] urls

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ decimals.prec = 18
 logger = logging.getLogger(__name__)
 
 class Config(object):
-    BASE_APP_SECRET_KEY = os.getenv("BASE_APP_SECRET_KEY")
-    DB_NAME = os.environ.get("POSTGRES_DB")
-    USERNAME = os.environ.get("POSTGRES_USER")
-    PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-    HOST = os.getenv("POSTGRES_HOST")
-    PORT = os.getenv("POSTGRES_PORT")
+    BASE_APP_SECRET_KEY = os.getenv("BASE_APP_SECRET_KEY", "django-insecure-d07ohkof%-t&2x0-&p8-d5+d0d9ph-)&ysqw!d43&*$%y$8gob")
+    DB_NAME = os.environ.get("POSTGRES_DB", "wallet_system")
+    USERNAME = os.environ.get("POSTGRES_USER", "postgres")
+    PASSWORD = os.environ.get("POSTGRES_PASSWORD", "mamedov00")
+    HOST = os.getenv("POSTGRES_HOST", "localhost")
+    PORT = os.getenv("POSTGRES_PORT", "5432")
