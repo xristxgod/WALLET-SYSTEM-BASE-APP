c10317404dcbe9d7b1bf82938e56daa1b89dfb1b
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-31 14:45:13 +0300

    [ADD] reg view

--- wallet/views/auth.py
+++ wallet/views/auth.py
@@ -32,3 +32,22 @@ class RegistrationView(View):
             'form': form,
         }
         return render(request, 'registration.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.username = form.cleaned_data['username']
+            new_user.email = form.cleaned_data['email']
+            new_user.first_name = form.cleaned_data['first_name']
+            new_user.last_name = form.cleaned_data['last_name']
+            new_user.save()
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            login(request, new_user)
+            return HttpResponseRedirect('/')
+        context = context = {
+            'form': form,
+        }
+        return render(request, 'registration.html', context)
