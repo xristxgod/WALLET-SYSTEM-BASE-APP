8a5c258f0dd519c8b080f20560bddb7ad5d5cb99
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-31 14:19:55 +0300

    [ADD] login

--- wallet/views/auth.py
+++ wallet/views/auth.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
 from wallet.forms.auth import LoginForm
@@ -10,4 +12,16 @@ class LoginView(View):
         context = {
             "form": form
         }
+        return render(request, 'login.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        context = {'form': form}
         return render(request, 'login.html', context)
