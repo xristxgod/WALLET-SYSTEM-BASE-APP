174959ecbbd34037bd004ff326be8bd27036131f
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-31 14:17:27 +0300

    [FIX] settings

--- base/settings.py
+++ base/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from config import Config
 
@@ -80,5 +81,17 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_dev'),
+)
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEPLATE_PACK = 'bootstrap5'
