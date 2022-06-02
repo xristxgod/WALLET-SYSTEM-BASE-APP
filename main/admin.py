2dbc052b122d0a70a3d7cfd666dacdb7f460b926
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-02 10:39:19 +0300

    [ADD] admin model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from main.models import UserModel
+
+admin.site.register(UserModel, UserAdmin)
