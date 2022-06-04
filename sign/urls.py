ea9af4c94d1781a44b3e2be32bdb269c2a8e14f0
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-04 08:28:06 +0300

    [ADD] logout view

--- sign/urls.py
+++ sign/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from sign.views.auth_views import LoginAuthenticationView, LoginAuthView
 from sign.views.reg_views import RegistrationView
 
 urlpatterns = [
     path("login/", LoginAuthenticationView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path("login/auth/<str:params>", LoginAuthView.as_view(), name="login_auth"),
     path("registration/", RegistrationView.as_view(), name="registration"),
 ]
